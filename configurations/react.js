2824b82fe96e8a3c022206e1df1339b52f6912cf
Timon van Spronsen <timonvanspronsen@outlook.com>
2017-07-14 12:12:13 +0200

    Update Standard and add Prettier

--- configurations/react.js
+++ configurations/react.js
@@ -1,12 +1,4 @@
 module.exports = {
-  extends: ['awkward/base', 'standard-react'],
-  parserOptions: {
-    ecmaVersion: 6,
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    'jsx-quotes': [2, 'prefer-single'],
-  },
+  extends: ['awkward/base', 'standard-react', 'prettier-react'],
+  plugins: ['prettier']
 }
