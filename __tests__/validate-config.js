f220f61b059d6a0296d63d5abe0d294bf6aed312
Timon van Spronsen <timonvanspronsen@outlook.com>
2017-07-14 13:57:34 +0200

    Validate react config

--- __tests__/validate-config.js
+++ __tests__/validate-config.js
@@ -2,12 +2,28 @@
 
 const eslint = require('eslint')
 
-test('load config in eslint to validate all rule syntax is correct', () => {
+test('load base config in eslint to validate all rule syntax is correct', () => {
   const CLIEngine = eslint.CLIEngine
 
   const cli = new CLIEngine({
     useEslintrc: false,
     configFile: '.eslintrc.json'
+  })
+
+  const code = 'var foo = 1\nvar bar = function() {}\nbar(foo)\n'
+
+  const res = cli.executeOnText(code)
+
+  expect(res.errorCount).toBe(0)
+  expect(res.warningCount).toBe(0)
+})
+
+test('load react config in eslint to validate all rule syntax is correct', () => {
+  const CLIEngine = eslint.CLIEngine
+
+  const cli = new CLIEngine({
+    useEslintrc: false,
+    configFile: 'react.js'
   })
 
   const code = 'var foo = 1\nvar bar = function() {}\nbar(foo)\n'
