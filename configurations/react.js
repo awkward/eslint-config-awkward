0abfaea870b4761787b08463ee320f9bb521f7a7
Timon van Spronsen <timonvanspronsen@outlook.com>
2017-07-18 14:21:32 +0200

    FIX: disable jsx-quotes rule

--- configurations/react.js
+++ configurations/react.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: [require.resolve('./base.js'), 'standard-react', 'prettier/react']
+  extends: [require.resolve('./base.js'), 'standard-react', 'prettier/react'],
+  rules: {
+    'jsx-quotes': 'off'
+  }
 }
